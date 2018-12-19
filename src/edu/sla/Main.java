772916db55ebf8c5c3e75318ceda133c3b3d7c7f
vhernandez <vhernandez@scienceleadership.org>
2018-12-19 01:19:03 -0500

    Add recursive countdown.

--- src/edu/sla/Main.java
+++ src/edu/sla/Main.java
@@ -3,11 +3,23 @@ package edu.sla;
 public class Main {
 
     public static void main(String[] args) {
-        // Let's launch 10 rockets
-        for (int i = 1; i <= 10; i++) {
-            Rocket rocket = new Rocket(String.valueOf(i));
+        boolean recursiveMode = true;
+        int numRockets = 10;
+
+        System.out.println("Will launch " + numRockets + " Rockets" + (recursiveMode ? " recursively." : " iteratively."));
+        System.out.println("");
+
+        // Let's launch some rockets
+        for (int i = 0; i < numRockets; i++) {
+            Rocket rocket = new Rocket(i+1, recursiveMode);
             rocket.run();
+
             // DON'T continue launching more rockets until this rocket is launched
+            if (!rocket.launched()) {
+                System.out.println("ERROR: Continuing to next rocket without launching current rocket!");
+            }
         }
+
+        System.out.println("All Rockets launched successfully!");
     }
 }
--- src/edu/sla/Rocket.java
+++ src/edu/sla/Rocket.java
@@ -1,23 +1,31 @@
 package edu.sla;
 
-public class Rocket implements Runnable {
-    String name;
-    int launchTime;
-    boolean canLaunch;
+class Rocket implements Runnable {
+    private int id;
+    private int launchTime;
+    private boolean canLaunch;
+    private boolean launched;
 
-    Rocket(String number) {
-        name = number;
-        // launchTime is random amount between 2 and 8
+    private boolean recursiveMode;
+
+    Rocket(int rocketId, boolean mode) {
+        id = rocketId;
+        // launchTime is random amount between 2 and 10 seconds
         launchTime = 2 + (int)(Math.random() * 8);
         canLaunch = false;
+        launched = false;
+        recursiveMode = mode;
     }
 
-    private void countdown() {
-        System.out.println("Rocket " + name + " countdown commence.");
-        for (int i = launchTime; i >= 1; i--) {
-            System.out.println("Rocket " + name + " launching in " + i + " seconds.");
+    boolean launched() {
+        return launched;
+    }
+
+    private void countdownIterative() {
+        for (int i = launchTime; i > 0; i--) {
+            System.out.println("Rocket " + id + " launching in " + i + " seconds.");
             try {
-                // wait 1 second (which is same as 1000 milliseconds
+                // wait 1 second (which is same as 1000 milliseconds)
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
                 Thread.currentThread().interrupt();
@@ -26,13 +34,38 @@ public class Rocket implements Runnable {
         canLaunch = true;
     }
 
+    private void countdownRecursive(int count) {
+        if (count > 0) {
+            System.out.println("Rocket " + id + " launching in " + count + " seconds.");
+            try {
+                // wait 1 second (which is same as 1000 milliseconds)
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+            countdownRecursive(count - 1);
+        } else {
+            canLaunch = true;
+        }
+    }
+
     private void launch() {
-            System.out.println("Rocket "+ name + " launching!!!!!!");
-            System.out.println("");
+        if (canLaunch) {
+            System.out.println("Rocket " + id + " launching!!!!!!");
+            launched = true;
+        } else {
+            System.out.println("ERROR: Rocket " + id + " can't launch BEFORE countdown");
+        }
+        System.out.println("");
     }
 
     public void run() {
-        countdown();
+        System.out.println("Rocket " + id + " countdown commence.");
+        if (recursiveMode) {
+            countdownRecursive(launchTime);
+        } else {
+            countdownIterative();
+        }
         launch();
     }
 }
